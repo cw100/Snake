dbd883667a6c4ee11c2b5f5cf3e9826923db0726
cw100 <connordixie@gameviewer.co.uk>
2015-01-15 11:31:40 +0000

    ScoreObject appears!

---
+++ Snake/Snake/ScoreObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    [Serializable]
+    public class ScoreObject
+    {
+        public int score;
+        public string username;
+    }
+}
